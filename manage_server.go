908731861815fb0dcb18e0895af72730eefd4f3e
Jonas Gunneng Heer <jonas.g.heer@gmail.com>
2019-10-09 21:45:36 +0200

    Wait for instance to get ready before requesting ipaddress

--- manage_server.go
+++ manage_server.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
-func startServer() {
+func startServer(tagValue string) {
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String("eu-west-1"),
 	})
@@ -20,63 +20,75 @@ func startServer() {
 	svc := ec2.New(sess)
 
 	// check if minecraft instance already exists
-	result, err := svc.DescribeInstances(&ec2.DescribeInstancesInput{
+	instancesInfo, err := svc.DescribeInstances(&ec2.DescribeInstancesInput{
 		Filters: []*ec2.Filter{
 			{
-				//Name:   aws.String("tag-key"),
-				//Values: []*string{aws.String("minecraft")},
 				Name:   aws.String("tag:Name"),
-				Values: []*string{aws.String("My first instance")},
+				Values: []*string{aws.String(tagValue)},
 			},
 		},
 	})
 	if err != nil {
 		fmt.Println("Error", err)
 	} else {
-		for _, reservation := range result.Reservations {
+		for _, reservation := range instancesInfo.Reservations {
 			for _, instance := range reservation.Instances {
+				if *instance.State.Name == "terminated" {
+					continue
+				}
 				for _, tag := range instance.Tags {
-					if *tag.Key == "Name" && *tag.Value == "My first instance" {
+					if *tag.Key == "Name" && *tag.Value == tagValue {
 						// panics if instance does not have public ip
-						fmt.Printf("Instance already exists: %s", *instance.PublicIpAddress)
-
+						fmt.Printf("Instance already exists: %s\n", *instance.PublicIpAddress)
+						return
 					}
 				}
 			}
 		}
-
-		//fmt.Println("Success", result.Reservations[0].Instances[0])
-		//fmt.Println(len(result.Reservations))
 	}
-	os.Exit(0)
-	/*
-		runResult, err := svc.RunInstances(&ec2.RunInstancesInput{
-			ImageId:      aws.String("ami-0ce71448843cb18a1"),
-			InstanceType: aws.String("t2.micro"),
-			MinCount:     aws.Int64(1),
-			MaxCount:     aws.Int64(1),
-		})
-		if err != nil {
-			fmt.Println("Could not create instance:", err)
-			return
-		}
 
-		fmt.Println("Created instance:", *runResult.Instances[0].InstanceId)
+	runResult, err := svc.RunInstances(&ec2.RunInstancesInput{
+		ImageId:      aws.String("ami-0ce71448843cb18a1"),
+		InstanceType: aws.String("t2.micro"),
+		MinCount:     aws.Int64(1),
+		MaxCount:     aws.Int64(1),
+	})
+	if err != nil {
+		fmt.Println("Could not create instance:", err)
+		return
+	}
+	mcInstanceId := *runResult.Instances[0].InstanceId
 
-		_, errtag := svc.CreateTags(&ec2.CreateTagsInput{
-			Resources: []*string{runResult.Instances[0].InstanceId},
-			Tags: []*ec2.Tag{
-				{
-					Key:   aws.String("Name"),
-					Value: aws.String("minecraft"),
-				},
+	err = svc.WaitUntilInstanceRunning(&ec2.DescribeInstancesInput{
+		InstanceIds: []*string{aws.String(mcInstanceId)},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	minecraftInstance, err := svc.DescribeInstances(&ec2.DescribeInstancesInput{
+		InstanceIds: []*string{aws.String(mcInstanceId)},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// does not exist yet, need to wait
+	fmt.Println("Created instance:", *minecraftInstance.Reservations[0].Instances[0].PublicIpAddress)
+
+	_, errtag := svc.CreateTags(&ec2.CreateTagsInput{
+		Resources: []*string{runResult.Instances[0].InstanceId},
+		Tags: []*ec2.Tag{
+			{
+				Key:   aws.String("Name"),
+				Value: aws.String(tagValue),
 			},
-		})
-		if errtag != nil {
-			log.Println("Could not create tags for instance:", runResult.Instances[0].InstanceId, errtag)
-			return
-		}
+		},
+	})
+	if errtag != nil {
+		log.Println("Could not create tags for instance:", runResult.Instances[0].InstanceId, errtag)
+		return
+	}
 
-		fmt.Println("Successfully tagged instance")
-	*/
+	fmt.Println("Successfully tagged instance")
 }
